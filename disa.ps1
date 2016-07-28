03a1b368bb202700d99a0e07e627e1589364eaeb
dagans <dagans@ford.com>
2016-07-28 10:47:50 -0400

    added a check and fix param

--- disa.ps1
+++ disa.ps1
@@ -7,9 +7,6 @@ This script has been created specifically to consume the DISA STIG for Suse Linu
 This script has been created specifically to consume the DISA STIG for Suse Linux.  Should probably work fine on other STIGs as well.  This script will allow you to view the XML contents using Powershell objects
 .PARAMETER xmlfile
 
-The script will attempt to determine the xml file to use, but if unable you will need to provide the XML file
-.PARAMETER article
-
 This parameter will be used to search for the article that you specify.  Required unless -listarticles is specified.
 .PARAMETER listarticles
 
@@ -17,29 +14,35 @@ List all of the articles in a summary view.  Can be used with -search_id to narr
 .PARAMETER search_id
 
 Can be either the full ID of the article or a partial, for use with listarticles
+.PARAMETER check
+
+Shows only the check info for the article
+.PARAMETER fix
+
+Shows only the fix info for the article
 .EXAMPLE
 
 ./disa.ps1 -listarticles
 .EXAMPLE
 
-./disa.ps1 -article 44877
+./disa.ps1 -search_id 44877
 #>
 
 # Need to set some type of default and then error based on that instead of
 # throwing an exception if no input is provided.  Maybe could list by default?
 Param(
     [string]$xmlfile,
-    [string]$article,
     [switch]$listarticles,
-    [string]$search_id
+    [string]$search_id,
+    [switch]$fix,
+    [switch]$check
 )
 
 # Figure out which xml file to use and then return the file as an object
 function Get-DISAFile {
-    
     $folder_xml_files = get-childitem .   | where-object {$_.name -like '*.xml'}
     $well_named_xml   = $folder_xml_files | where-object {$_.name -like '*STIG*'}
-    
+
     if ($xmlfile) {
         if (test-path $xmlfile) {
             return (get-item $xmlfile)
@@ -58,34 +61,45 @@ function Get-DISAFile {
 # function to provide a summary-style list of objects
 function List-DISA {
     $disa_items = @()
-    foreach ($article in $xml_file_contents.Benchmark.group.rule) {
-      $article_description = $article.description.Replace("<VulnDiscussion>","")
+    foreach ($search_id in $xml_file_contents.Benchmark.group.rule) {
+      $article_description = $search_id.description.Replace("<VulnDiscussion>","")
       $vuln_tag_location = $article_description.indexof('</VulnDiscussion>')
       $article_description = $article_description.Substring(0,$vuln_tag_location)
       # remove that revision string at the end
-      $article_id = $article.id.replace("r1_rule","")
+      $article_id = $search_id.id.replace("r1_rule","")
       $article_id = $article_id.replace("r2_rule","")
       $article_id = $article_id.replace("r3_rule","")
       $current_item = New-Object -TypeName PSObject
       $current_item | Add-Member -MemberType NoteProperty -Name 'id' -Value $article_id
-      $current_item | Add-Member -MemberType NoteProperty -Name 'title' -Value $article.title
+      $current_item | Add-Member -MemberType NoteProperty -Name 'title' -Value $search_id.title
       $current_item | Add-Member -MemberType NoteProperty -Name 'description' -Value $article_description
       $disa_items += $current_item
     }
-      return $disa_items | where-object {$_.id -like "*$search_id*"}
+      return $disa_items
+}
+
+function Fix-Text {
+    $complete_text = ($xml_file_contents.Benchmark.group.rule | where-object {$_.id -like "*$search_id*"}).fixtext."#text"
+    $cmd_pos = $complete_text.indexof('#')
+    $fix_command = $complete_text.substring($cmd_pos)
+    write-host $fix_command
+    $fix_obj = New-Object -TypeName psobject
+    $fix_obj | Add-Member -MemberType NoteProperty -Name 'fix_text' -Value $complete_text
+    $fix_obj | Add-Member -MemberType NoteProperty -Name 'Command' -Value $fix_command
+    return $fix_obj
 }
 
 # gets a more detailed view of a single item
 function Get-Article {
-    if ($article) {
-        $current_item = $xml_file_contents.Benchmark.group.rule | select id,version,title,description,name,namespaceuri,fixtext,check | where-object {$_.id -like "*$article*"}
+    if ($search_id) {
+        $current_item = $xml_file_contents.Benchmark.group.rule | select id,version,title,description,name,namespaceuri,fixtext,check | where-object {$_.id -like "*$search_id*"}
         if ($current_item.count -gt 1) {throw "More than one item found.  Please use complete article number rather than partial"}
         $article_result = New-Object -TypeName psobject
         $article_id = $current_item.id.replace("r1_rule","")
         $article_result | Add-Member -MemberType NoteProperty -Name 'id' -Value $article_id
         $article_result | Add-Member -MemberType NoteProperty -Name 'title' -Value $current_item.title
         $article_description = $current_item.description.Replace("<VulnDiscussion>","")
-        $vuln_tag_location = $article_description.indexof('</VulnDiscussion>')
+        $vuln_tag_location = $article_description.indexof("</VulnDiscussion>")
         $article_description = $article_description.Substring(0,$vuln_tag_location)
         $article_result | Add-Member -MemberType NoteProperty -Name 'description' -Value $article_description
         $article_result | Add-Member -MemberType NoteProperty -Name 'version' -value $current_item.version
@@ -107,7 +121,10 @@ $xml_file = Get-DISAFile
 # If -listarticles is used provide a summary view of the search
 if ( $PSBoundParameters.ContainsKey('listarticles')) {
     List-DISA
-# If -listarticle is not used we will run for a detailed view of a single article
+} elseif ($PSBoundParameters.ContainsKey('check')) {
+    (Get-Article).check
+} elseif ($PSBoundParameters.ContainsKey('fix')) {
+    (Get-Article).fixtext
 } else {
     Get-Article
 }
